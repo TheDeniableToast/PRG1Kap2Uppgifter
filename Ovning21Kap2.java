0672b555ba75b21317055d8682ddf9ebfd068f4e
Filip Norberg <filip.norberg@learnet.se>
2019-09-13 11:18:19 +0200

    added files

---
+++ Ovning21Kap2.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ovning21Kap2 {
+public static void main(String[] args) {
+     String namn;
+	 namn = "Filip Norberg";
+	 String adress;
+	 adress = "Hultesvägen 45";
+	 String nummer;
+	 nummer = "0727038628";
+	 String allt;
+	 allt = namn + "\n" + adress + "\n" + nummer;
+     JOptionPane.showMessageDialog(null, allt);
+}
+}
